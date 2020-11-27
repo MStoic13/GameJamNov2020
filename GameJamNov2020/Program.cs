394587237eba948b1c1f98804c6e38cb4a8ca099
MStoic13 <34403668+MStoic13@users.noreply.github.com>
2020-11-26 19:22:00 -0800

    init commit

---
+++ GameJamNov2020/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameJamNov2020
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameJamNov2020())
+                game.Run();
+        }
+    }
+}
